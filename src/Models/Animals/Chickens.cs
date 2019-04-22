d0487b4cb5f5fb7cd54e8687a04805d918cccfe4
jacob sanders <jkehsanders@gmail.com>
2019-04-22 11:31:47 -0500

    Started purchaseSeeds ended making animals

---
+++ src/Models/Animals/Chickens.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using Trestlebridge.Interfaces;
+
+namespace Trestlebridge.Models.Animals {
+    public class Chicken : IResource, IMeatProducing {
+
+        private Guid _id = Guid.NewGuid();
+        private double _meatProduced = 1.7;
+
+        public string Type {get; } = "Chicken";
+
+        private string _shortId {
+            get {
+                return this._id.ToString().Substring(this._id.ToString().Length - 6);
+            }
+        }
+
+        public double Butcher () {
+            return _meatProduced;
+        }
+
+        public override string ToString () {
+            return $"Chicken {this._shortId}. Cluck Cluck Cluck!";
+        }
+    }
+}
