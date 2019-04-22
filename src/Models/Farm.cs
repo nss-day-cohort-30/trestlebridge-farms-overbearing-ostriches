2307f59930fa1c4ba9a3bece26f0a9b1a3f74b72
Stephen_Clark <stephenc369@gmail.com>
2019-04-22 11:37:22 -0500

    added facilities and animal lists

--- src/Models/Farm.cs
+++ src/Models/Farm.cs
@@ -8,35 +8,47 @@ namespace Trestlebridge.Models
 {
     public class Farm
     {
-        public List<GrazingField> GrazingFields { get; } = new List<GrazingField>();
+        public List<GrazingField> GrazingFieldList { get; } = new List<GrazingField>();
+        public List<PlowedField> PlowedFieldList { get; } = new List<PlowedField>();
+
+        public List<ChickenHouse> ChickenHouseList { get; } = new List<ChickenHouse>();
+
+        public List<DuckHouse> DuckHouseList { get; } = new List<DuckHouse>();
+
+        public List<NaturalField> NaturalFieldList { get; } = new List<NaturalField>();
 
         /*
             This method must specify the correct product interface of the
             resource being purchased.
          */
-        public void PurchaseResource<T> (IResource resource, int index)
+        public void PurchaseResource<T>(IResource resource, int index)
         {
             Console.WriteLine(typeof(T).ToString());
             switch (typeof(T).ToString())
             {
                 case "Cow":
-                    GrazingFields[index].AddResource((IGrazing)resource);
+                    GrazingFieldList[index].AddResource((IGrazing)resource);
                     break;
                 default:
                     break;
             }
         }
 
-        public void AddGrazingField (GrazingField field)
+        public void AddGrazingField(GrazingField field)
         {
-            GrazingFields.Add(field);
+            GrazingFieldList.Add(field);
+        }
+
+        public void AddPlowedField(PlowedField field)
+        {
+            PlowedFieldList.Add(field);
         }
 
         public override string ToString()
         {
             StringBuilder report = new StringBuilder();
 
-            GrazingFields.ForEach(gf => report.Append(gf));
+            GrazingFieldList.ForEach(gf => report.Append(gf));
 
             return report.ToString();
         }
