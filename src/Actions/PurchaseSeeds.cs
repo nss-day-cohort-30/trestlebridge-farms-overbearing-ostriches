6c4d7ffe59930c451acc1170c96f84db77e0c7bc
Stephen_Clark <stephenc369@gmail.com>
2019-04-22 13:45:06 -0500

    fix missing Using

--- src/Actions/PurchaseSeeds.cs
+++ src/Actions/PurchaseSeeds.cs
@@ -1,21 +1,23 @@
 using System;
 using Trestlebridge.Interfaces;
 using Trestlebridge.Models;
+using Trestlebridge.Models.Animals;
 
 namespace Trestlebridge.Actions
 {
     public class PurchaseSeeds
     {
-        public static void CollectInput (Farm farm) {
+        public static void CollectInput(Farm farm)
+        {
             Console.WriteLine("1. Sunflower Seeds");
             Console.WriteLine("2. Wildflower Seeds");
             Console.WriteLine("3. Sesame Seeds");
 
-            Console.WriteLine ();
-            Console.WriteLine ("What are you buying today?");
+            Console.WriteLine();
+            Console.WriteLine("What are you buying today?");
 
-            Console.Write ("> ");
-            string choice = Console.ReadLine ();
+            Console.Write("> ");
+            string choice = Console.ReadLine();
 
             switch (Int32.Parse(choice))
             {
