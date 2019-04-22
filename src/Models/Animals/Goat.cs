89962dae2f4c8787e7df7adac226270ef793dcfe
Stephen_Clark <stephenc369@gmail.com>
2019-04-22 13:06:23 -0500

    update

--- src/Models/Animals/Goat.cs
+++ src/Models/Animals/Goat.cs
@@ -2,14 +2,18 @@ using System;
 using System.Collections.Generic;
 using Trestlebridge.Interfaces;
 
-namespace Trestlebridge.Models.Animals {
-    public class Goats : IResource, IGrazing, IMeatProducing {
+namespace Trestlebridge.Models.Animals
+{
+    public class Goat : IResource, IGrazing, IMeatProducing
+    {
 
         private Guid _id = Guid.NewGuid();
         private double _meatProduced = 7.5;
 
-        private string _shortId {
-            get {
+        private string _shortId
+        {
+            get
+            {
                 return this._id.ToString().Substring(this._id.ToString().Length - 6);
             }
         }
@@ -18,15 +22,18 @@ namespace Trestlebridge.Models.Animals {
         public string Type { get; } = "Goat";
 
         // Methods
-        public void Graze () {
+        public void Graze()
+        {
             Console.WriteLine($"Goat {this._shortId} just ate {this.GrassPerDay}kg of grass");
         }
 
-        public double Butcher () {
+        public double Butcher()
+        {
             return _meatProduced;
         }
 
-        public override string ToString () {
+        public override string ToString()
+        {
             return $"Goat {this._shortId}. Baaaaa!";
         }
     }
