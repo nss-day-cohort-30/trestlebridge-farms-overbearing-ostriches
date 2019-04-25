388f1bdcb81f23e6a6d6efd73fff4b8a86528276
jacob sanders <jkehsanders@gmail.com>
2019-04-25 10:20:33 -0500

    can add several seeds at once

--- src/Actions/ChoosePlowedPlantingField.cs
+++ src/Actions/ChoosePlowedPlantingField.cs
@@ -3,21 +3,25 @@ using System.Linq;
 using Trestlebridge.Interfaces;
 using Trestlebridge.Models;
 using Trestlebridge.Models.Plants;
+using System.Collections.Generic;
+using Trestlebridge.Models.Facilities;
 
 namespace Trestlebridge.Actions
 {
 
     public class ChoosePlowedPlantingField
     {
-        public static void CollectInput(Farm farm, IPlowedFieldFlower PlowedFieldFlower)
+        public static void CollectInput(Farm farm, List<Sesame> sesameSeeds)
         {
             Console.Clear();
 
             for (int i = 0; i < farm.PlowedFieldList.Count; i++)
             {
-                var plowedField = farm.PlowedFieldList[i];
-                Console.WriteLine($"{i + 1}. Plowed Field");
-                plowedField.listResources();
+                 dynamic sesameSeedField = farm.PlantFacilityList[i];
+                {
+                    Console.WriteLine($"{i + 1}. {sesameSeedField.GetType().Name} ");
+                    sesameSeedField.listResources();
+                }
             }
 
             Console.WriteLine();
@@ -28,7 +32,10 @@ namespace Trestlebridge.Actions
             int choice = Int32.Parse(Console.ReadLine());
 
             choice = choice - 1;
-            farm.PlowedFieldList[choice].AddPlantResource(PlowedFieldFlower);
+            dynamic chosenField = farm.PlantFacilityList[choice];
+
+            var sesameSeedsForPlowedField = sesameSeeds.Cast<IPlowedFieldFlower>().ToList();
+            chosenField.AddPlantResource(sesameSeedsForPlowedField);
         }
     }
 }
